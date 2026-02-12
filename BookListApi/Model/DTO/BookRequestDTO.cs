a526ae20f566e965ca2e702c087db78f7345396c
runarer <95697611+runarer@users.noreply.github.com>
2026-02-12 14:40:55 +0100

    Response and request objects

---
+++ BookListApi/Model/DTO/BookRequestDTO.cs
@@ -0,0 +1,3 @@
+namespace BookListApi.Model.DTO;
+
+public record BookRequestDTO(string Name, string ISBN, string Author, bool Read, string? Universe, string? Series);
---
+++ BookListApi/Model/DTO/BookResponseDTO.cs
@@ -0,0 +1,3 @@
+namespace BookListApi.Model.DTO;
+
+public record BookResponseDTO(int Id, string Name, string ISBN, string Author, bool Read, string? Universe, string? Series);
