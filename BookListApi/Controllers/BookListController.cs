18ce5020e5590fbb4c3d0bdb2a260a11390a9f92
runarer <95697611+runarer@users.noreply.github.com>
2026-02-12 14:48:17 +0100

    Adds controller class
    Implement BookService before continuing

---
+++ BookListApi/Controllers/BookListController.cs
@@ -0,0 +1,12 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookListController() : ControllerBase
+{
+    //TODO Add Endpoints here
+}
