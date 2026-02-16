8c31ffefc00bc61dcee2178a1e478db423db0d7b
runarer <95697611+runarer@users.noreply.github.com>
2026-02-16 14:12:26 +0100

    Adds validation annotations

--- BookListApi/Model/DTO/BookRequestDTO.cs
+++ BookListApi/Model/DTO/BookRequestDTO.cs
@@ -1,3 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BookListApi.Model.DTO;
 
-public record BookRequestDTO(string Name, string ISBN, string Author, bool? Read, string? Universe, string? Series, int PositionInSeries);
+public record BookRequestDTO(
+    [Required]
+    string Name,
+
+    [Required]
+    string ISBN,
+
+    [Required]
+    string Author,
+
+    bool? Read,
+
+    string? Universe,
+
+    string? Series,
+
+    [Range(0,int.MaxValue)]
+    int PositionInSeries
+);
