82f5941d5963e22a08ac849181b385b9881726fd
runarer <95697611+runarer@users.noreply.github.com>
2026-02-16 14:43:00 +0100

    Use the correct PATCH instead of GET
    GET should not alter the state of something on the server, and since it's just a small change PATCH seams more accurate than PUT.

--- BookListApi/Controllers/BookListController.cs
+++ BookListApi/Controllers/BookListController.cs
@@ -25,12 +25,12 @@ public class BookListController(BookListService service) : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetAsync(int id) => await service.Get(id) is BookResponseDTO book ? Ok(book) : NotFound();
 
-    [HttpGet("{id:int}/read")]
+    [HttpPatch("{id:int}/read")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> BookReadAsync(int id) => await service.Completed(id, true) is BookResponseDTO book ? Ok(book) : NotFound();
 
-    [HttpGet("{id:int}/not-read")]
+    [HttpPatch("{id:int}/not-read")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> BookNotReadAsync(int id) => await service.Completed(id, false) is BookResponseDTO book ? Ok(book) : NotFound();
